44dc526753df2e8ffbab087ee7900aa82b89b057
Harvesh Kumar <techieharsh1991@gmail.com>
2018-03-24 00:12:36 +0530

    edit moderator page

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -2,4 +2,25 @@ class Admin::ModeratorsController < ApplicationController
   def index
     @moderators = Moderator.all
   end
+
+  def edit
+    @moderator = Moderator.find(params[:id])
+  end
+
+  def update
+    @moderator = Moderator.find(params[:id])
+    if @moderator.update(moderator_params)
+      flash[:notice] = "Moderator was successfully updated."
+      redirect_to admin_moderators_path
+    else
+      flash[:alert] = "There was a problem updating moderator."
+      render 'edit'
+    end
+  end
+
+  private
+
+  def moderator_params
+    params.require(:moderator).permit(:id, :fullname, :username, :password)
+  end
 end
--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -2,4 +2,8 @@ class Moderator < ApplicationRecord
   has_secure_password
 
   has_many :posts
+
+  validates :fullname, presence: true
+  validates :username, presence: true, format: {with: /@/, message: 'is not valid'}
+  validates :password, presence: true
 end
