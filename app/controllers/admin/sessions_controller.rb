587da72a32b229544d9513beeb035310f5d3f4a9
Harvesh Kumar <techieharsh1991@gmail.com>
2018-03-24 00:36:20 +0530

    create sessions controller and files

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Admin::SessionsController < Admin::ApplicationController
+  before_action :authorize, except: [:new, :create]
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get '/login' => 'admin/sessions#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
 end
