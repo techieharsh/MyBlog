0f5b51eca6ef7a723f65e42f62d126b524f4e407
Harvesh Kumar <techieharsh1991@gmail.com>
2018-04-10 13:48:23 +0530

    Dashboard forntend page completed successfully.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < Admin::ApplicationController
   def index
     @posts= Post.last 5
     @comments = Comment.last 5
-    @visitors = Visitor.last 5
+    @visitors = Visitor.last 10
   end
 end
