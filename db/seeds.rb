d2638736e21c93ad6b0e043171e81466bbe11276
Harvesh Kumar <techieharsh1991@gmail.com>
2018-04-11 12:13:09 +0530

    tag admin page completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,46 +12,46 @@ moderator = Moderator.create(
   password: "12345678"
 )
 
-50.times do
-
-  post = Post.create(
-    title: Faker::Lorem.sentence(20),
-    content: Faker::Lorem.paragraph,
-    publish: true,
-    moderator: moderator
-  )
-
-  tag = Tag.create(
-    name: Faker::Lorem.word
-  )
-
-  post_tag = PostTag.create(post: post,
-    tag: tag
-  )
-
-  visitor = Visitor.create(
-    fullname: Faker::Name.name,
-    email: Faker::Internet.email
-  )
-
-  comment = Comment.create(
-    message: Faker::Lorem.paragraph,
-    status: [true, false].sample,
-    post: post,
-    visitor: visitor
-  )
-
-  message = Message.create(
-    content: Faker::Lorem.paragraph,
-    status: [true, false].sample,
-    visitor: visitor
-  )
-
-  notifiable = [visitor, comment].sample
-
-  notification = Notification.create(
-    notifiable_id: notifiable.id,
-    notifiable_type: notifiable.class.name
-  )
-
-end
+# 50.times do
+#
+#   post = Post.create(
+#     title: Faker::Lorem.sentence(20),
+#     content: Faker::Lorem.paragraph,
+#     publish: true,
+#     moderator: moderator
+#   )
+#
+#   tag = Tag.create(
+#     name: Faker::Lorem.word
+#   )
+#
+#   post_tag = PostTag.create(post: post,
+#     tag: tag
+#   )
+#
+#   visitor = Visitor.create(
+#     fullname: Faker::Name.name,
+#     email: Faker::Internet.email
+#   )
+#
+#   comment = Comment.create(
+#     message: Faker::Lorem.paragraph,
+#     status: [true, false].sample,
+#     post: post,
+#     visitor: visitor
+#   )
+#
+#   message = Message.create(
+#     content: Faker::Lorem.paragraph,
+#     status: [true, false].sample,
+#     visitor: visitor
+#   )
+#
+#   notifiable = [visitor, comment].sample
+#
+#   notification = Notification.create(
+#     notifiable_id: notifiable.id,
+#     notifiable_type: notifiable.class.name
+#   )
+#
+# end
